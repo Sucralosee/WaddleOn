11863c43e450ed8e9b16035e98c34a27685b6050
Ben Louis <benlouis360@gmail.com>
2024-03-25 13:27:58 -0700

    moved things around

--- pages/onboard.js
+++ pages/onboard.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Landing from "@/components/Landing";
 
 export default function Home() {
     return (
@@ -11,7 +12,9 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                
+                <div>
+                    <Landing/>
+                </div>
             </main>
         </>
     )
