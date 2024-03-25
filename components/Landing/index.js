a0c2cbe7aebb586a7ab3507da4d5f64286e9c390
Ben Louis <benlouis360@gmail.com>
2024-03-25 09:31:41 -0700

    first commit

---
+++ components/Landing/index.js
@@ -0,0 +1,13 @@
+
+
+export default function Landing () {
+
+    return (
+        <>
+            <div>
+                hi
+            </div>
+        </>
+    )
+
+}
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import Landing from "@/components/Landing";
+
 
 export default function Home() {
   return (
@@ -11,6 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
+          <div>
+            <Landing/>
+          </div>
       </main>
     </>
   );
