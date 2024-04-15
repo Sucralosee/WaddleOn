a9fc1c6dfad0e0c4da876d8f9a7eb8849fe13ed6
Ben Louis <benlouis360@gmail.com>
2024-04-15 09:20:12 -0700

    added images and button to cycle progress

---
+++ components/Progress/Progress.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import styles from "./Progress.module.css";
+import Image from "next/image";
+
+
+export default function Progress() {
+    const [stepNum, setstepNum] = useState(1);
+
+    const nextStep = () => {
+        if (stepNum < 8) {
+            setstepNum(stepNum + 1);
+        }
+    };
+
+    return (
+        <div className={styles.progressContainer}>
+            <Image 
+                className={styles.pondImage}
+                src={`/images/Steps/step${stepNum}.png`}
+                width={400}
+                height={400}
+                alt={`dark pond indicating step ${stepNum}`}                
+            />
+            <button onClick={nextStep} className={styles.nextButton}>
+                Continue
+            </button>
+        </div>
+    );
+}
--- pages/onboard.js
+++ pages/onboard.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/onboard.module.css";
 import Landing from "@/components/Landing";
+import Progress from "@/components/Progress/Progress";
 
 export default function Home() {
     return (
@@ -12,8 +13,8 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main>
-                <div>
-                    <Landing/>
+                <div className={styles.onboardContainer}>
+                    <Progress/>
                 </div>
             </main>
         </>
